186286c9e49690c665d3a08dff55b1dd6687d962
lavanya <tlavanya619@gmail.com>
2026-05-09 11:36:45 +0530

    Fixed correct backend URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://ai-app-generator-clqi.onrender.com/api",
+  baseURL: "https://ai-app-generator-c1qi.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
